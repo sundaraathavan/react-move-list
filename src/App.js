cf919b56e71291be89bfa69f19f3384815bd5c26
athavan <52910050+sundaraathavan@users.noreply.github.com>
2022-05-14 17:30:33 -0700

    master

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
 
     return (
         <div className="app">
-            <h1>MovieLand</h1>
+            <h1>Athavan MovieLand</h1>
 
             <div className="search">
                 <input 
